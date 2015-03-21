4cb45341af93c8ec8458557ae400266cd893b247
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2015-03-21 15:54:18 +0700

    contract use

--- app/NewsLetterV3.php
+++ app/NewsLetterV3.php
@@ -1,14 +1,18 @@
 <?php namespace App;
 
 use App\Contracts\MailListContract;
+use Illuminate\Contracts\Logging\Log;
 class NewsLetterV3 {
 	protected $mailList;
+	protected $logger;
 
-	public function __construct(MailListContract $mailList)
+	public function __construct(MailListContract $mailList, Log $logger)
 	{
 		$this->mailList = $mailList;
+		$this->logger = $logger;
 	}
 	public function register($email) {
+		$this->logger->info('User baru diregistrasi : ' . $email);
 		return $this->mailList->register($email);
 	}
 }
